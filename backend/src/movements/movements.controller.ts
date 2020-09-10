b5952523d813ee0756f700272488e94f32104f47
Jordan Chong <jordanchong@Jordans-Work-MBP.local>
2020-09-10 10:09:58 -0700

    Add dynamic route using route parameters

--- backend/src/movements/movements.controller.ts
+++ backend/src/movements/movements.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('movements')
 export class MovementsController {
   @Get()
   findAll() {
     return 'this route returns all movements';
+  }
+
+  // @Get(':id')
+  // findOne(@Param() params) {
+  //   return `this route returns ${params.id} movement`;
+  // }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `this route returns ${id} movement`;
   }
 }
