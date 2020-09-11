abe151e9a7c289963e358613fb93d39e4624892f
Jordan Chong <jordanchong@Jordans-Work-MBP.local>
2020-09-11 12:17:30 -0700

    Add url query params to findAll and add Patch/Delete endpoints
    Also clean up and remove demo code

--- backend/src/movements/movements.controller.ts
+++ backend/src/movements/movements.controller.ts
@@ -1,20 +1,18 @@
-import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus, Res, Patch, Delete, Query } from '@nestjs/common';
 import { create } from 'domain';
 
 @Controller('movements')
 export class MovementsController {
   @Get()
-  findAll(@Res() response) {
-    // Since nest is using express by default, we can utilze any method standard to the
-    // express library using the response. this feature allows some added flexiblity by allowing
-    // full control of the response object for header manipulation or library specific features,
-    // it should be used with caution. Doing this breaks compatibility with Nest features that depend
-    // on Nest standard response handling (such as interceptors and the HTTPCode decorator) It also makes
-    // our code platform dependent and harder to test (since we'd need to mock the response object) 
-    // ie:
-      // response.status(200).send('this route returns all movements...')
-    
-    return 'this route returns all movements';
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    // try visit: localhost:3001/movements?limit=20&offset=10
+    return `this route returns all movements. Limit ${limit}, offset ${offset}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `this route returns ${id} movement`;
   }
 
   // @Get(':id')
@@ -22,14 +20,18 @@ export class MovementsController {
   //   return `this route returns ${params.id} movement`;
   // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `this route returns ${id} movement`;
-  }
-
   @Post()
-  @HttpCode(HttpStatus.GONE) // This is an example of what we might want to do if we were going to deprecate this endpoint
   create(@Body() body: any){
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: 'string', @Body() body) {
+    return `this route updates ${id} movement`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: 'string', @Body() body) {
+    return `this route removes ${id} movement`;
   }
 }
