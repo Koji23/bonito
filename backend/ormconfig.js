9ad99b9045b89b2b0f03d00d67b3b017664dca4e
Jordan Chong <jordan@aerofs.com>
2020-10-20 22:28:58 -0700

    Enable running migrations locally
    Ports have been remapped to be in the 4000's for all containers.
    This avoids port mapping conflicts when the bonito dev environment
    is running alongside other containers. This also somehow allows
    the typeorm migrations to be run locally without having to exec
    into the backend container.

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,7 +1,13 @@
 module.exports = {
   type: 'postgres',
-  host: 'postgres-db',
-  port: 5432,
+  // since migrations are easier to run locally using npx, we are specifying localhost here.
+  // If the migration is to be run after exec'ing into a container, then replace this with the
+  // name of the postgres container "postgres-db"
+  // see https://docs.docker.com/compose/networking/
+  host: 'localhost',
+  // 4004 is the localport assigned to the postgres container. Use the container's port 5432
+  // when running the migration from inside the container
+  port: 4004,
   username: 'postgres',
   password: 'abc123',
   database: 'bonito',
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,12 +10,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
       host: 'postgres-db', // database host. ie: localhost. But here Docker-compose will sub in the service name of our postgres database. TODO: change this to env variable
-      port: 5432, // database port
+      port: 5432, // database port (uses the container's port not the host's port)
       username: 'postgres', // username
       password: 'abc123', // user password, *see docker-compose.yml
       database: 'bonito', // name of our database, *see docker-compose.yml
       autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
-      synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (TODO: disable in the production). This means nest will automatically generate a SQL table for all classes with the @Entity decorator
+      synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (TODO: disable in the production). This means nest will automatically generate a SQL table for all classes with the @Entity decorator. This will undo any migrations that aren't aren't mirrored in the entities
     }),
   ],
   controllers: [AppController],
